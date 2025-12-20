cd5d1d5ab92105b22bcc6a9f391fdf4b7dea6761
deveshh30 <devesh30gupta@gmail.com>
2025-12-21 02:03:06 +0530

    connected succesfully

--- work-1/src/DB/index.js
+++ work-1/src/DB/index.js
@@ -2,15 +2,18 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../utils/constants.js";
 
 const connectdb = async () => {
-    try {
-        const connectionInstant = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        console.log(`mongo database is connected succesfully !! DB HOST = ${connectionInstant)`;
-        
-    } catch (error) {
-        console.log("ERROR in connnection", error);
-        process.exit(1)
-        
-    }
-}
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+
+    console.log(
+      `MongoDB connected successfully !! DB HOST = ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.error("ERROR in DB connection:", error);
+    process.exit(1);
+  }
+};
 
 export default connectdb;
--- work-1/src/utils/index.js
+++ work-1/src/utils/index.js
@@ -1,23 +1,8 @@
+console.log("ENV CHECK:", process.env.MONGODB_URL);
 
-import dotenv from "dotenv"
-import connectdb from "./Users/devesh/Documents/BACKEND/work-1/src/index.js"; 
 
-dotenv.config({
-    path : './env'
-})
+import connectdb from "../DB/index.js"
+
 
 connectdb()
 
-
-
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./constants";
-// ( async () => {
-//     try {
-//        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-//     } catch (error) {
-//         console.log("ERROR" : error);
-//         throw error
-
-//     }
-// } ) ()
