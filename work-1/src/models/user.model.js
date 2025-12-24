a6ac5fd02364d588b8ed47dc0a6bbe86bb8a90c4
deveshh30 <devesh30gupta@gmail.com>
2025-12-25 01:19:41 +0530

    setup project successfully

--- work-1/src/models/user.model.js
+++ work-1/src/models/user.model.js
@@ -90,7 +90,7 @@ userSchema.methods.generateAccessToken = function () {
 
 userSchema.methods.generateRefreshToken = function () {
     return jwt.sign(
-        {
+        { 
         _id : this.id,
         
 
