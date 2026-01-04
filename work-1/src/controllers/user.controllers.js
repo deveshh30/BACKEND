1203a1fffb6dc785c241fdf62259e901be3d93d3
deveshh30 <devesh30gupta@gmail.com>
2026-01-04 21:34:19 +0530

    project structure

--- work-1/src/controllers/user.controllers.js
+++
@@ -1,9 +0,0 @@
-import {asyncHandler} from "../utils/AsyncHandler.js"
-
-const registerUser = asyncHandler( async(req, res) => {
-    res.status(200).json({
-        message : "ok"
-    })
-} )
-
-export {registerUser}
