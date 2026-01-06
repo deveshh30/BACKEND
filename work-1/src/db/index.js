c19f07a01639a812268ff5f444b1bc7970541a40
deveshh30 <devesh30gupta@gmail.com>
2026-01-06 19:48:43 +0530

    conneted db

--- work-1/src/db/index.js
+++ work-1/src/db/index.js
@@ -1,14 +1,14 @@
-import mongoose, { connect } from "mongoose";
+import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`the connted data base name is ${process.env.MONGODB_URL}/${DB_NAME} `)
-        console.log(`\n db connect hogya !! DB HOST = ${connectionInstance}`);
-        
-    } catch (error) {
-        console.log("database connection error", error);
-        process.exit(1)
-        
-    }
-}
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- work-1/src/index.js
+++ work-1/src/index.js
@@ -1,8 +1,27 @@
-import mongoose from "mongoose";
-import {DB_NAME} from "./constants.js";
+// require('dotenv').config({path: "./env"}) 
+// this line start all env variable as soon as file opens
+
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config();
 
 const app = express();
+
+await connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server listening on port ${process.env.PORT || 3000}`);
+});
+
+dotenv.config({
+    path: './env'
+})
 
 
 // ( async () => {
